944a78ad464afb2b262e4a2621696050bd16f380
Marc Jou <smyslov@gmail.com>
2023-01-31 08:06:00 +0100

    Add vanilla emacs inits

---
+++ emacs.d/init.el
@@ -0,0 +1,23 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
+;; Install use-package
+(straight-use-package 'use-package)
+
+;; Configure use-package to use straight.el by default
+(use-package straight
+  :custom (straight-use-package-by-default t))
+
+(straight-use-package 'org)
+(org-babel-load-file (expand-file-name "~/.emacs.d/org-init.org"))
