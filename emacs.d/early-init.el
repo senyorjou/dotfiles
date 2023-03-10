728d63889a002e2adf15814c770824ba00ef10fe
Marc Jou <smyslov@gmail.com>
2023-03-10 08:20:58 +0100

    Add proper emacs.d dir

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -1,2 +1,4 @@
+;; improve startup time by pausing garbage collection during init
+(setq gc-cons-threshold most-positive-fixnum)
 ;; Disable package.el in favor of straight.el
 (setq package-enable-at-startup nil)
