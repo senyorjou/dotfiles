054b107d48f8e3bf7690b5def5ca145707bf1f33
Marc Jou <mjou@usertesting.com>
2023-10-12 14:35:42 +0200

    Add some m1 conf

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,15 +21,15 @@
 
 (straight-use-package 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/org-init.org"))
-;; set GC collector 
+;; set GC collector
 (setq gc-cons-threshold (* 2 1000 1000))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default)))
+ '(package-vc-selected-packages
+   '((sandcastle-theme :vc-backend Git :url "https://github.com/habamax/sandcastle-theme"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
