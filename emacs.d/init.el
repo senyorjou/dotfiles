919472e9f201794209e1010153d9b0b78b41ac20
Marc Jou <mjou@usertesting.com>
2023-10-12 15:03:45 +0200

    Add doom-oksolar theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -28,8 +28,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-vc-selected-packages
-   '((sandcastle-theme :vc-backend Git :url "https://github.com/habamax/sandcastle-theme"))))
+ '(custom-safe-themes
+   '("c5878086e65614424a84ad5c758b07e9edcf4c513e08a1c5b1533f313d1b17f1" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
