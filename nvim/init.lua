5d611adc02602f0a951a2eda4a2eb57b4a4ef915
Marc Jou <smyslov@gmail.com>
2024-09-18 08:41:38 +0200

    Add neovim colors

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.o.background = "light" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
+-- vim.o.background = "light" -- or "light" for light mode
+-- vim.cmd([[colorscheme gruvbox]])
